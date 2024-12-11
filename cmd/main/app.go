686fe598fd72431de021697b042cae511fa8dd4c
RadKirill00 <Radkirill00@mail.ru>
2024-12-11 19:20:27 +0300

    Сделал легкий веб сервер

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,9 +1,36 @@
 package main
 
 import (
- 
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
 )
 
+
+func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+   name := params.ByName("name")
+   w.Write([]byte(name))
+}
+
 func main() {
-	
+    router := httprouter.New()
+
+    router.GET("/:name", IndexHandler)
+
+    listen, err := net.Listen("tcp", ":1234")   
+
+     if err!= nil {
+        log.Fatal(err)
+    }
+    
+    server := &http.Server{
+        Handler: router,
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout: 15 * time.Second, 
+    }
+    
+    log.Fatal(server.Serve(listen))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module api
 
 go 1.23.2
+
+require github.com/julienschmidt/httprouter v1.3.0
